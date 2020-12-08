1c9d8027b90d9ec4faa6270bc8f997a5c9ba1d53
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-12-08 16:27:23 +0530

    fix https://github.com/fawazahmed0/quran/issues/13

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const editionsLink = apiLink + '/editions'
 
 const defaultEdition = 'eng-miraneesuddin'
 
+const CHAPTER_LENGTH = 114
+
 let editionsJSON
 
 // call this only once
@@ -70,7 +72,7 @@ async function createDropdown () {
   for (const [value, key, dir] of langEdition) { $('#translationdropdown').append('<option value="' + key + '" data-dir="' + dir + '">' + value + '</option>') }
 
   // Create chapter dropdown
-  for (let i = 1; i <= 114; i++) { $('#chapter').append('<option value="' + i + '">' + i + '</option>') }
+  for (let i = 1; i <= CHAPTER_LENGTH; i++) { $('#chapter').append('<option value="' + i + '">' + i + ' - '+ arabicChapters[i-1]+' ('+englishChapters[i-1]+ ')</option>') }
 }
 
 window.showTranslations = async function showTranslations () {
@@ -159,21 +161,24 @@ const chaplength = [7, 286, 200, 176, 120, 165, 206, 75, 129, 109, 123, 111, 43,
 const mappings = []
 const mappingsStr = []
 
-for (let i = 1; i <= 114; i++) {
+for (let i = 1; i <= CHAPTER_LENGTH; i++) {
   for (let j = 1; j <= chaplength[i - 1]; j++) {
     mappings.push([i, j])
     mappingsStr.push(i + ',' + j)
   }
 }
 
+
+
+let chaptersJSON = {"Al-Faatiha":"The Opening","Al-Baqara":"The Cow","Aal-i-Imraan":"The Family of Imraan","An-Nisaa":"The Women","Al-Maaida":"The Table","Al-An'aam":"The Cattle","Al-A'raaf":"The Heights","Al-Anfaal":"The Spoils of War","At-Tawba":"The Repentance","Yunus":"Jonas","Hud":"Hud","Yusuf":"Joseph","Ar-Ra'd":"The Thunder","Ibrahim":"Abraham","Al-Hijr":"The Rock","An-Nahl":"The Bee","Al-Israa":"The Night Journey","Al-Kahf":"The Cave","Maryam":"Mary","Taa-Haa":"Taa-Haa","Al-Anbiyaa":"The Prophets","Al-Hajj":"The Pilgrimage","Al-Muminoon":"The Believers","An-Noor":"The Light","Al-Furqaan":"The Criterion","Ash-Shu'araa":"The Poets","An-Naml":"The Ant","Al-Qasas":"The Stories","Al-Ankaboot":"The Spider","Ar-Room":"The Romans","Luqman":"Luqman","As-Sajda":"The Prostration","Al-Ahzaab":"The Clans","Saba":"Sheba","Faatir":"The Originator","Yaseen":"Yaseen","As-Saaffaat":"Those drawn up in Ranks","Saad":"The letter Saad","Az-Zumar":"The Groups","Ghafir":"The Forgiver","Fussilat":"Explained in detail","Ash-Shura":"Consultation","Az-Zukhruf":"Ornaments of gold","Ad-Dukhaan":"The Smoke","Al-Jaathiya":"Crouching","Al-Ahqaf":"The Dunes","Muhammad":"Muhammad","Al-Fath":"The Victory","Al-Hujuraat":"The Inner Apartments","Qaaf":"The letter Qaaf","Adh-Dhaariyat":"The Winnowing Winds","At-Tur":"The Mount","An-Najm":"The Star","Al-Qamar":"The Moon","Ar-Rahmaan":"The Beneficent","Al-Waaqia":"The Inevitable","Al-Hadid":"The Iron","Al-Mujaadila":"The Pleading Woman","Al-Hashr":"The Exile","Al-Mumtahana":"She that is to be examined","As-Saff":"The Ranks","Al-Jumu'a":"Friday","Al-Munaafiqoon":"The Hypocrites","At-Taghaabun":"Mutual Disillusion","At-Talaaq":"Divorce","At-Tahrim":"The Prohibition","Al-Mulk":"The Sovereignty","Al-Qalam":"The Pen","Al-Haaqqa":"The Reality","Al-Ma'aarij":"The Ascending Stairways","Nooh":"Noah","Al-Jinn":"The Jinn","Al-Muzzammil":"The Enshrouded One","Al-Muddaththir":"The Cloaked One","Al-Qiyaama":"The Resurrection","Al-Insaan":"Man","Al-Mursalaat":"The Emissaries","An-Naba":"The Announcement","An-Naazi'aat":"Those who drag forth","Abasa":"He frowned","At-Takwir":"The Overthrowing","Al-Infitaar":"The Cleaving","Al-Mutaffifin":"Defrauding","Al-Inshiqaaq":"The Splitting Open","Al-Burooj":"The Constellations","At-Taariq":"The Morning Star","Al-A'laa":"The Most High","Al-Ghaashiya":"The Overwhelming","Al-Fajr":"The Dawn","Al-Balad":"The City","Ash-Shams":"The Sun","Al-Lail":"The Night","Ad-Dhuhaa":"The Morning Hours","Ash-Sharh":"The Consolation","At-Tin":"The Fig","Al-Alaq":"The Clot","Al-Qadr":"The Power, Fate","Al-Bayyina":"The Evidence","Az-Zalzala":"The Earthquake","Al-Aadiyaat":"The Chargers","Al-Qaari'a":"The Calamity","At-Takaathur":"Competition","Al-Asr":"The Declining Day, Epoch","Al-Humaza":"The Traducer","Al-Fil":"The Elephant","Quraish":"Quraysh","Al-Maa'un":"Almsgiving","Al-Kawthar":"Abundance","Al-Kaafiroon":"The Disbelievers","An-Nasr":"Divine Support","Al-Masad":"The Palm Fibre","Al-Ikhlaas":"Sincerity","Al-Falaq":"The Dawn","An-Naas":"Mankind"};
+let arabicChapters = Object.keys(chaptersJSON)
+let englishChapters = Object.values(chaptersJSON)
+
 // Call initializer function in the beginning itself, to fetch all necessary JSON's
 let initVar
 window.addEventListener('DOMContentLoaded', (event) => {
   initVar = oneTimeFunc()
 })
-
-
-
 /*
 function getDirection (str) {
   const divelem = document.createElement('div')
