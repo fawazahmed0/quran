f09591b294fe1f7b9908bddea924326e2573d9eb
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-07-17 05:30:09 +0000

    build on code content

--- copydir.js
+++ copydir.js
@@ -1,4 +1,4 @@
 const fs = require('fs-extra')
 const path = require('path')
-fs.copySync(path.join(__dirname, 'code','list'), path.join(__dirname, 'dist','list'))
+fs.copySync(path.join(__dirname, 'code'), path.join(__dirname, 'dist'))
 //fs.copySync(path.join(__dirname, 'blog'), path.join(__dirname, 'build','docsdata','blog'))
